8317c3703b88b71870f7b1acb12b00fb8a994792
Ria Verma <rverma35@gatech.edu>
2015-09-26 20:22:04 -0400

    add folder

---
+++ Archive/my_cool_app.js
@@ -0,0 +1,26 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+      var inputs = document.getElementsByTagName("input");
+      for (var i = 0; i < inputs.length; i++) {
+          alert(inputs[i].id);
+      }
+  });
+}
